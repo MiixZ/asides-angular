57ac5c00c4369cf477d5079d82591c79afd1e3a5
ismael <ismael.diazdiaz@keifitech.com>
2024-05-13 13:09:23 +0200

    configuración final tailwind

---
+++ src/app/layout-c/layout-c.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LayoutCComponent } from './layout-c.component';
+
+describe('LayoutCComponent', () => {
+  let component: LayoutCComponent;
+  let fixture: ComponentFixture<LayoutCComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LayoutCComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(LayoutCComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/layout-c/layout-c.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-c',
+  templateUrl: './layout-c.component.html',
+  styleUrl: './layout-c.component.css'
+})
+export class LayoutCComponent {
+
+}
